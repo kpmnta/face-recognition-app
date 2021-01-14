13412222392f0e0fda09cc8207361583c71184a8
kai <kai.pimenta1@gmail.com>
2021-01-14 17:03:03 -0300

    front end projeto

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = () => {
+    return (
+        <>
+            <div className="white f3">
+                {'Kai, your current rank is '}
+            </div>
+            <div className="white f1">
+                {'#1'}
+            </div>
+        </>
+    );
+}
+
+export default Rank;
